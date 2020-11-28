bbe6f159dc90a7a3d5ba85b8823c209d35d9b15d
Ryan RAJKOMAR <ryan.rajkomar@itspire.fr>
2020-11-28 16:16:17 +0100

    Updated Structure and dependencies
    Renamed Enumeration to Enum
    Added new response code for "Unavailable For Legal Reasons"
    Fixed readme comment && Updated licensing headers && Removed unnecessary comments
    Updated composer dependencies && Added roave/security-advisories

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Itspire - HTTP Common Classes
+# Itspire - HTTP Common
 
 This library follows the PSR-12 convention.
--- src/main/php/Enum/HttpMethod.php
+++ src/main/php/Enum/HttpMethod.php
@@ -1,40 +1,25 @@
 <?php
 
-/**
- * Copyright (c) 2016 - 2019 Itspire.
+/*
+ * Copyright (c) 2016 - 2020 Itspire.
  * This software is licensed under the BSD-3-Clause license. (see LICENSE.md for full license)
  * All Right Reserved.
  */
 
 declare(strict_types=1);
 
-namespace Itspire\Http\Common\Enumeration;
+namespace Itspire\Http\Common\Enum;
 
-use Itspire\Common\Enumeration\AbstractEnumeration;
+use Itspire\Common\Enum\AbstractEnum;
 
-class HttpMethod extends AbstractEnumeration
+class HttpMethod extends AbstractEnum
 {
-    /** @var string GET */
     public const GET = 'GET';
-
-    /** @var string POST */
     public const POST = 'POST';
-
-    /** @var string PUT */
     public const PUT = 'PUT';
-
-    /** @var string PATCH */
     public const PATCH = 'PATCH';
-
-    /** @var string DELETE */
     public const DELETE = 'DELETE';
-
-    /** @var string HEAD */
     public const HEAD = 'HEAD';
-
-    /** @var string OPTIONS */
     public const OPTIONS = 'OPTIONS';
-
-    /** @var string CONNECT */
     public const CONNECT = 'CONNECT';
 }
